c9473ba69a348b5037ee3b67373a5fe0f58d6320
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-09 21:42:22 -0300

    Adicionar arquivos de projeto.

---
+++ PlanoTelefonico/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int minutos;
+            double conta;
+
+            Console.WriteLine("Entre com os minutos");
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100)
+            {
+                conta += (minutos - 100) * 2.0;
+            }
+
+            Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+        }
+    }
+}
